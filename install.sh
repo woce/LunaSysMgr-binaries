3d0a2fab670627bb32f73daa526a2eb61f765f66
Josh Palmer <jpalmerwatkins@me.com>
2012-09-04 22:11:38 +0100

    LunaCE 4.7.0

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/useCustomCarrierString.json < ./tweaks/useCustomCarrierString.json
+novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/spread-gestures.json < ./tweaks/spread-gestures.json
 novacom run -- file:///sbin/stop LunaSysMgr
 novacom run file://bin/mount -- -o remount,rw /
 novacom put file:///usr/bin/LunaSysMgr < ./bin/LunaSysMgr
