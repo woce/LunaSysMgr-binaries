d07dee5d007f13eed60180b87afba7906c3a681e
Josh Palmer <jpalmerwatkins@me.com>
2012-09-05 22:34:48 +0100

    Add mini-cards.json to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/useCustomCarrierString.json < ./tweaks/useCustomCarrierString.json
 novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/spread-gestures.json < ./tweaks/spread-gestures.json
+novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/spread-gestures.json < ./tweaks/mini-cards.json
 novacom run -- file:///sbin/stop LunaSysMgr
 novacom run file://bin/mount -- -o remount,rw /
 novacom put file:///usr/bin/LunaSysMgr < ./bin/LunaSysMgr
