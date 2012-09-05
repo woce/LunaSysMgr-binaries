a06ad0453c601b318e5d97b049782a9f8823808f
Josh Palmer <jpalmerwatkins@me.com>
2012-09-05 22:49:07 +0100

    More install.sh fixing

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.twe
 novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/spread-gestures.json < ./tweaks/spread-gestures.json
 novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/spread-gestures.json < ./tweaks/mini-cards.json
 novacom run -- file:///sbin/stop LunaSysMgr
-sleep 1
 novacom run file://bin/mount -- -o remount,rw /
+sleep 1
 novacom put file:///usr/bin/LunaSysMgr < ./bin/LunaSysMgr
 novacom run file://bin/mount -- -o remount,ro /
 novacom run -- file:///sbin/reboot
