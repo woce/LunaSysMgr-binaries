9cc45ab029dc244d55f65400be485621b94e923d
Donald Kirker <donald.kirker@openmobl.com>
2012-09-08 17:25:49 -0700

    Update install script to invoke the Tweaks install

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/useCustomCarrierString.json < ./tweaks/useCustomCarrierString.json
-novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/spread-gestures.json < ./tweaks/spread-gestures.json
-novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/mini-cards.json < ./tweaks/mini-cards.json
-novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/zoom-gestures.json < ./tweaks/zoom-gestures.json
-novacom put file:///media/cryptofs/apps/usr/palm/services/org.webosinternals.tweaks.prefs/preferences/wave-launcher.json < ./tweaks/wave-launcher.json
+./install_tweaks.sh
+
+echo "Installing LunaSysMgr ..."
 novacom run -- file:///sbin/stop LunaSysMgr
 novacom run file://bin/mount -- -o remount,rw /
 sleep 1
